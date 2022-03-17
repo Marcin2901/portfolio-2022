0881357b3a1744ffd5a3cee57da937b63c1ceb87
Marcin <jamarcin2901@wp.pl>
2022-03-17 14:59:17 +0100

    change package.json file

--- src/Page/Projects/Projects.js
+++ src/Page/Projects/Projects.js
@@ -3,6 +3,8 @@ import "./Projects.css";
 import {Parallax} from "react-parallax"
 import Aos from "aos";
 import "aos/dist/aos.css";
+import fbProjectImg from "../../images/fb-project.jpg";
+import casinoProjectImg from "../../images/casino-project.jpg";
 
 
 function Projects() {
@@ -20,33 +22,46 @@ function Projects() {
         <section className="projects">
     
 
+            <h2>Projekty</h2>
             
             <div data-aos="fade-up" className="container projects__container">
      
-
           
                         <div data-aos="fade-right" className="project--card" >
-                            <h4>Project Title</h4>
-                            <p>akls; sakdl;f  ksdajf; sdfk ;lskdjf k sdf; 
-                                ksadl fkjfsdjsfhdjhsa flsd f aks;djf 
-                                k asdf;kjsakdfj;klsjadf kl dsjfa;k sdfa k;jl
-                            </p>
+                            <img className="project--img" src={fbProjectImg} />
+                            <div>
+                                <h4>Facebook Clone</h4>
+                                <p>Projekt stworzony za pomocą biblioteki React, z możliwością
+                                tworzenia nowego konta, wysyłania wiadomości, wstawiania postów
+                                i komentarzy
+                                </p>
+                                <div>
+                                    <a href="">Github</a>
+                                    <a href="https://marcin2901.github.io/facebook-clone/">Zobacz project</a>
+                                </div>
+                            </div>
                         </div>
          
-                        <div data-aos="flip-up" className="project--card"> 
+                        {/* <div data-aos="flip-up" className="project--card"> 
                             <h4>Project T</h4>
                             <p>akls; sakdl;f  ksdajf; sdfk ;lskdjf k sdf; 
                                 ksadl fkjfsdjsfhdjhsa flsd f aks;djf 
                                 k asdf;kjsakdfj;klsjadf kl dsjfa;k sdfa k;jl
                             </p>
-                        </div>
+                        </div> */}
 
                         <div data-aos="fade-left" className="project--card" >
-                            <h4>Project </h4>
-                            <p>akls; sakdl;f  ksdajf; sdfk ;lskdjf k sdf; 
-                                ksadl fkjfsdjsfhdjhsa flsd f aks;djf 
-                                k asdf;kjsakdfj;klsjadf kl dsjfa;k sdfa k;jl
-                            </p>
+                            <img className="project--img" src={casinoProjectImg} />
+                            <div>
+                                <h4>Mini Casino</h4>
+                                <p>Projekt stworzony za pomocą biblioteki React, z możliwością 
+                                granie w Poker, Poker 5 Cards, BlackJack i War
+                                </p>
+                                <div>
+                                    <a href="">Github</a>
+                                    <a href="https://marcin2901.github.io/mini-casino/">Zobacz project</a>
+                                </div>
+                            </div>
                         </div>
         
        
